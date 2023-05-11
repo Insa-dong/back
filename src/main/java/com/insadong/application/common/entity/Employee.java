f95866d590e9ad8b323d75a34173fb266f482d5a
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-05-11 11:11:48 +0900

    empEntity

--- src/main/java/com/insadong/application/common/entity/Employee.java
+++ src/main/java/com/insadong/application/common/entity/Employee.java
@@ -1,14 +1,61 @@
 package com.insadong.application.common.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "TB_EMP")
 public class Employee {
 	@Id
 	@Column(name = "EMP_CODE")
-	public Long empCode;
+	private Long empCode;
+
+	@Column(name = "DEPT_CODE")
+	private String deptCode;
+
+	@Column(name = "JOB_CODE")
+	private String jobCode;
+
+	@Column(name = "EMP_NAME")
+	private String empName;
+
+	@Column(name = "EMP_GENDER")
+	private String empGender;
+
+	@Column(name = "EMP_EMAIL")
+	private String empEmail;
+
+	@Column(name = "EMP_ID")
+	private String empId;
+
+	@Column(name = "EMP_PWD")
+	private String empPwd;
+
+	@Column(name = "EMP_PHONE")
+	private String empPhone;
+
+	@Column(name = "HIRE_DATE")
+	private Date hireDate;
+
+	@Column(name = "OFF_COUNT")
+	private Long offCount;
+
+	@Column(name = "EMP_STATE")
+	private String empState;
+
+	@Column(name = "END_DATE")
+	private Date endDate;
+
+
 }
+
+
+
