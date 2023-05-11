bc36247e8108ee08ca878c62b98fa81dcb561d10
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-05-11 14:42:45 +0900

    오류 수정

--- src/main/java/com/insadong/application/employee/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.insadong.application.employee;
-
-import com.insadong.application.common.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
