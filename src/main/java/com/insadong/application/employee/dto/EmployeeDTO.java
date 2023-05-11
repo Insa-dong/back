8d3e3de4ab245cfef4a89f518978f2b65413406c
imyueol <imyueol000@gmail.com>
2023-05-11 10:57:11 +0900

    commit

--- src/main/java/com/insadong/application/employee/dto/EmployeeDTO.java
+++ src/main/java/com/insadong/application/employee/dto/EmployeeDTO.java
@@ -1,8 +1,27 @@
 package com.insadong.application.employee.dto;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
 public class EmployeeDTO {
-	public Long empCode;
+
+	private Long empCode;
+	private String deptCode;
+	private String jobCode;
+	private String empName;
+	private String empGender;
+	private String email;
+	private String empId;
+	private String empPwd;
+	private String empPhone;
+	private Date hireDate;
+	private Long offCount;
+	private String empState;
+	private Date endDate;
+	
 }
