057fac41bb93d38109164fe86201a732b9693c1f
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-05-12 15:37:27 +0900

    commit

--- src/main/java/com/insadong/application/common/entity/EmpAuth.java
+++ src/main/java/com/insadong/application/common/entity/EmpAuth.java
@@ -1,24 +1,21 @@
 package com.insadong.application.common.entity;
 
-import java.io.Serializable;
-
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "TB_EMP_AUTH")
 public class EmpAuth implements Serializable {
-	
+
 	@EmbeddedId
 	private EmpAuthPK empAuthPK;
-	
+
 	@ManyToOne
-	@JoinColumn(name="AUTH_CODE", insertable=false, updatable=false)
+	@JoinColumn(name = "AUTH_CODE", insertable = false, updatable = false)
 	private Auth auth;
 }
