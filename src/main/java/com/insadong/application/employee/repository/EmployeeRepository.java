d02580762b32e9451a8165ef2ab96bf91ed7a7ff
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-05-14 21:44:45 +0900

    push

--- src/main/java/com/insadong/application/employee/repository/EmployeeRepository.java
+++ src/main/java/com/insadong/application/employee/repository/EmployeeRepository.java
@@ -1,14 +1,14 @@
 package com.insadong.application.employee.repository;
 
 import com.insadong.application.common.entity.Employee;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+	@EntityGraph(attributePaths = {"dept", "job", "empAuthList"})
 	Optional<Employee> findByEmpId(String empId);
 
 
