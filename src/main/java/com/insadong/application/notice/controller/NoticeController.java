8be3815c3f8c86fce60974981caa22127d876f54
imyueol <imyueol000@gmail.com>
2023-05-17 14:04:36 +0900

    공지사항 조회 완료

---
+++ src/main/java/com/insadong/application/notice/controller/NoticeController.java
@@ -0,0 +1,80 @@
+package com.insadong.application.notice.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.insadong.application.common.ResponseDTO;
+import com.insadong.application.emporg.dto.EmpDTO;
+import com.insadong.application.notice.dto.NoticeDTO;
+import com.insadong.application.notice.service.NoticeService;
+import com.insadong.application.paging.Pagenation;
+import com.insadong.application.paging.PagingButtonInfo;
+import com.insadong.application.paging.ResponseDTOWithPaging;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/insa/v1")
+public class NoticeController {
+
+	private final NoticeService noticeService;
+
+	public NoticeController(NoticeService noticeService) {
+		this.noticeService = noticeService;
+	}
+
+	/* 공지사항 전체목록 조회 */
+	@GetMapping("/noticelist")
+	public ResponseEntity<ResponseDTO> selectNoticeList(@RequestParam(name = "page", defaultValue = "1") int page) {
+		log.info("[NoticeController] : selectNoticeList start ==================================== ");
+		log.info("[NoticeController] : page : {}", page);
+
+		Page<NoticeDTO> noticeDTOList = noticeService.selectNoticeList(page);
+
+		PagingButtonInfo pageInfo = Pagenation.getPagingButtonInfo(noticeDTOList);
+
+		log.info("[NoticeController] : pageInfo : {}", pageInfo);
+
+		ResponseDTOWithPaging responseDTOWithPaging = new ResponseDTOWithPaging();
+		responseDTOWithPaging.setPageInfo(pageInfo);
+		responseDTOWithPaging.setData(noticeDTOList.getContent());
+
+		log.info("[NoticeController] : selectNoticeList end ==================================== ");
+
+		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "조회 성공", responseDTOWithPaging));
+	}
+
+	/* 공지사항 검색조건 조회 */
+	@GetMapping("/noticesearch")
+	public ResponseEntity<ResponseDTO> searchNoticeByOption(
+			@RequestParam(name = "page", defaultValue = "1") int page,
+			@RequestParam(name = "searchOption") String searchOption,
+			@RequestParam(name = "searchKeyword") String searchKeyword) {
+
+		log.info("[NoticeController] : searchNoticeByOption start ==================================== ");
+		log.info("[NoticeController] : page : {}", page);
+		log.info("[NoticeController] : searchOption : {}", searchOption);
+		log.info("[NoticeController] : searchKeyword : {}", searchKeyword);
+
+		Page<NoticeDTO> noticeDTOList = noticeService.searchNoticeByOption(page, searchOption, searchKeyword);
+
+		PagingButtonInfo pageInfo = Pagenation.getPagingButtonInfo(noticeDTOList);
+
+		log.info("[NoticeController] : pageInfo : {}", pageInfo);
+
+		ResponseDTOWithPaging responseDTOWithPaging = new ResponseDTOWithPaging();
+		responseDTOWithPaging.setPageInfo(pageInfo);
+		responseDTOWithPaging.setData(noticeDTOList.getContent());
+
+		log.info("[NoticeController] : searchNoticeByOption end ==================================== ");
+
+		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "조회 성공", responseDTOWithPaging));
+	}
+
+}
