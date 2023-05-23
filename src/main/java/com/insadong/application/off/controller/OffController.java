9a89f7c0f9efbaf872407315eb8fc0fde65d535b
heyw00 <heywoo12@gmail.com>
2023-05-23 15:22:39 +0900

    ✨ Feat: 연차 신청 작성중

--- src/main/java/com/insadong/application/off/controller/OffController.java
+++ src/main/java/com/insadong/application/off/controller/OffController.java
@@ -5,24 +5,20 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.insadong.application.common.ResponseDTO;
 import com.insadong.application.employee.dto.EmployeeDTO;
 import com.insadong.application.off.dto.OffDTO;
 import com.insadong.application.off.service.OffService;
-import com.insadong.application.paging.Pagenation;
-import com.insadong.application.paging.PagingButtonInfo;
-import com.insadong.application.paging.ResponseDTOWithPaging;
 
 
 @RestController
@@ -38,34 +34,31 @@ public class OffController {
 		
 	/*1. 연차 신청*/
     @PostMapping("/apply")
-    public ResponseEntity<String> applyOff(@RequestBody OffDTO offDTO) {
-        offService.applyOff(offDTO);
-        return ResponseEntity.ok("신청 완료");
+    public ResponseEntity<ResponseDTO> applyOff(@RequestBody OffDTO offDTO, @AuthenticationPrincipal EmployeeDTO loggedInUser) {
+    	// 로그인한 구성원이 신청자
+        offDTO.setSignRequester(loggedInUser);
+
+        offService.applyOff(offDTO, loggedInUser);
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "조회 성공", offDTO));
     }
 
 		
 	/*3. 예정 연차 조회*/	
     @GetMapping("/my-comingUp-off")
-    public ResponseEntity<ResponseDTO> myComingUpOffList(@AuthenticationPrincipal EmployeeDTO emp,
-	                                                        @RequestParam(name = "page", defaultValue = "1") int page) {
+    public ResponseEntity<ResponseDTO> myComingUpOffList(@AuthenticationPrincipal EmployeeDTO emp) {
     	
-    	Page<OffDTO> offPage  = offService.myComingUpOffList(emp.getEmpCode(), page);
+    	List<OffDTO> offDTOList = offService.myComingUpOffList(emp.getEmpCode());
     	
     	// offEnd가 오늘 이후인 항목만 필터링
         LocalDate today = LocalDate.now();
         
-        List<OffDTO> offDTOList = offPage.getContent().stream()
+        offDTOList = offDTOList.stream()
                 .filter(off -> off.getOffEnd().isAfter(today) || off.getOffEnd().isEqual(today))
                 .collect(Collectors.toList());
+       
         
-	    PagingButtonInfo pageInfo = Pagenation.getPagingButtonInfo(offPage);
 
-	    ResponseDTOWithPaging responseDTOWithPaging = new ResponseDTOWithPaging();
-	    responseDTOWithPaging.setPageInfo(pageInfo);
-	    responseDTOWithPaging.setData(offDTOList);
-	
-
-	    return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "조회 성공", responseDTOWithPaging));
+	    return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "조회 성공", offDTOList));
     	
     }
     
--- src/main/java/com/insadong/application/off/service/OffService.java
+++ src/main/java/com/insadong/application/off/service/OffService.java
@@ -8,95 +8,91 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
+
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.insadong.application.common.entity.Employee;
 import com.insadong.application.common.entity.Off;
+import com.insadong.application.employee.dto.EmployeeDTO;
 import com.insadong.application.off.dto.OffDTO;
 import com.insadong.application.off.repository.EmpOffRepository;
 import com.insadong.application.off.repository.OffRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class OffService {
-	
+
 	public static final Long TOTAL_OFF_LEAVE = 15L;
 	private final OffRepository offRepository;
 	private final EmpOffRepository empOffRepository;
 	private final ModelMapper modelMapper;
-	
+
 	public OffService(OffRepository offRepository, EmpOffRepository empOffRepository, ModelMapper modelMapper) {
 		this.offRepository = offRepository;
 		this.empOffRepository = empOffRepository;
 		this.modelMapper = modelMapper;
 	}
 
-
 	/* 연차 신청 */
-	public void applyOff(OffDTO offDTO) {
-       
+	@Transactional
+	public void applyOff(OffDTO offDTO, EmployeeDTO loggedInUser) {
+
 		Off off = new Off();
 
 		LocalDate offStart = offDTO.getOffStart();
-	    LocalDate offEnd = offDTO.getOffEnd();
+		LocalDate offEnd = offDTO.getOffEnd();
 
-	    List<String> signStatusList = Arrays.asList("승인", "대기");
+		List<String> signStatusList = Arrays.asList("승인", "대기");
 
-	    // 이미 해당 날짜 범위에 신청된 연차가 있는지 확인
-	    if (offRepository.existsByOffStartLessThanEqualAndOffEndGreaterThanEqualAndSignStatusIn(offStart, offEnd, signStatusList)) {
-	        throw new RuntimeException("이미 해당 날짜에 연차를 신청했습니다.");
-	    }
+		// 이미 해당 날짜 범위에 승인, 대기 상태인 연차 신청이 있는지 확인. 
+		if (offRepository.existsByOffStartLessThanEqualAndOffEndGreaterThanEqualAndSignStatusIn(offStart, offEnd,
+				signStatusList)) {
+			throw new RuntimeException("이미 해당 날짜에 연차를 신청했습니다.");
+		}
 
-	    off.setOffStart(offStart);
-	    off.setOffEnd(offEnd);
-	    off.setOffDiv(offDTO.getOffDiv());
-	    off.setSignReason(offDTO.getSignReason());
-	    off.setSignStatus(offDTO.getSignStatus());
+		off.setOffStart(offStart);
+		off.setOffEnd(offEnd);
+		off.setOffDiv(offDTO.getOffDiv());
+		off.setSignReason(offDTO.getSignReason());
+		off.setSignStatus("대기"); // 고정값
 
-	    // 연차 일수 계산
-	    long days = ChronoUnit.DAYS.between(offStart, offEnd) + 1;
+		// 연차 일수 계산
+		long days = ChronoUnit.DAYS.between(offStart, offEnd) + 1;
 
-	    // 최대 1일로 제한
-	    double offDay = offDTO.getOffDiv().contains("반차") ? 0.5 : Math.min(days, 1);
-	    off.setOffDay(offDay);
-
-	    // 신청자 설정
-	    Employee requester = empOffRepository.getOne(offDTO.getSignRequester().getEmpCode());
+		// 반차면 0.5개로
+		double offDay = offDTO.getOffDiv().contains("반차") ? 0.5 : days;
+		off.setOffDay(offDay);
+	
+		// 신청자 설정
+	    Employee requester = modelMapper.map(loggedInUser, Employee.class);
 	    off.setSignRequester(requester);
+	
+		// 결재자 = 신청자 부서 팀장
+		Employee payer = empOffRepository.findTeamLeaderByDept(requester.getDept());
+		off.setSignPayer(payer);
 
-	    // 결재자 = 신청자 팀장
-	    Employee payer = empOffRepository.findTeamLeaderByDept(requester.getDept());
-	    off.setSignPayer(payer);
+		// 신청일 설정
+		LocalDate requestDate = LocalDate.now();
+		off.setRequestDate(Date.from(requestDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
 
-	    // 신청일 설정
-	    off.setRequestDate(Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+		offRepository.save(off);
 
-	    offRepository.save(off);
-	    
 	}
-
 
 	/* 3. 예정 연차 조회 */
-	public Page<OffDTO> myComingUpOffList(Long empCode, int page) {
-		PageRequest pageRequest = PageRequest.of(page -1 , 5, Sort.by(Sort.Direction.ASC, "offStart"));
-	    Page<Off> offList =  offRepository.findBySignRequester_EmpCode(empCode, pageRequest);
-		
-	    List<OffDTO> offDTOList = offList.getContent().stream()
-	            .map(off -> modelMapper.map(off, OffDTO.class))
-	            .collect(Collectors.toList());
+	public List<OffDTO> myComingUpOffList(Long empCode) {
+		List<Off> offList = offRepository.findBySignRequester_EmpCode(empCode, Sort.by("offStart"));
+		log.info("offList : {} ", offList);
 
-	    return new PageImpl<>(offDTOList, pageRequest, offList.getTotalElements());
-	    
+		List<OffDTO> offDTOList = offList.stream().map(off -> modelMapper.map(off, OffDTO.class))
+				.collect(Collectors.toList());
+
+		return offDTOList;
+
 	}
 }
-
-
-
-
-
-
-
