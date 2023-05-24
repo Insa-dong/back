ae9fa03fbc8226fb6239352db0dafb659bbc495c
kimtaehun521 <rth0204@gmail.com>
2023-05-24 22:14:10 +0900

    JsonFormat 추가

--- src/main/java/com/insadong/application/attend/dto/AttendDTO.java
+++ src/main/java/com/insadong/application/attend/dto/AttendDTO.java
@@ -2,6 +2,7 @@ package com.insadong.application.attend.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.insadong.application.student.dto.StudentDTO;
 import com.insadong.application.study.dto.StudyDTO;
 
@@ -13,6 +14,7 @@ public class AttendDTO {
 	private Long attendCode;
 	private StudyDTO study;
 	private StudentDTO student;
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date attendDate;
 	private String attendStatus;
 	
