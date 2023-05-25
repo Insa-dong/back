b4564d920f73c589779fef680f6383815f022959
imyueol <imyueol000@gmail.com>
2023-05-25 15:39:56 +0900

    파일 다운로드 완료

--- src/main/java/com/insadong/application/notice/controller/NoticeController.java
+++ src/main/java/com/insadong/application/notice/controller/NoticeController.java
@@ -1,7 +1,13 @@
 package com.insadong.application.notice.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,9 +36,11 @@ import lombok.extern.slf4j.Slf4j;
 public class NoticeController {
 
 	private final NoticeService noticeService;
+	private final ResourceLoader resourceLoader;
 
-	public NoticeController(NoticeService noticeService) {
+	public NoticeController(NoticeService noticeService, ResourceLoader resourceLoader) {
 		this.noticeService = noticeService;
+		this.resourceLoader = resourceLoader;
 	}
 
 	/* 공지사항 전체목록 조회 */
@@ -81,32 +89,55 @@ public class NoticeController {
 
 		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "조회 성공", responseDTOWithPaging));
 	}
-	
-	
 
-	/* 공지사항 등록 */
+	/* 공지사항 등록(파일 업로드) */
 	@PostMapping("/noticeregist")
-	public ResponseEntity<ResponseDTO> registNotice(@ModelAttribute NoticeDTO noticeDTO, @AuthenticationPrincipal EmpDTOImplUS employeeDTO) throws IOException {
-		
+	public ResponseEntity<ResponseDTO> registNotice(@ModelAttribute NoticeDTO noticeDTO,
+			@AuthenticationPrincipal EmpDTOImplUS employeeDTO) throws IOException {
+
 		log.info("[NoticeController] noticeDTO: {}", noticeDTO);
 		log.info("[NoticeController] empDTO: {}", employeeDTO);
-		
+
 //		noticeDTO.setNoticeWriter(employeeDTO);
-		
+
 		noticeService.registNotice(noticeDTO, employeeDTO.getEmpCode());
 
 		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "공지사항 등록 성공"));
 	}
-	
-	
-	/* 공지사항 상세 조회*/
+
+	/* 공지사항 상세 조회 */
 	@GetMapping("/notice/{noticeCode}")
 	public ResponseEntity<ResponseDTO> selectNoticeList(@PathVariable Long noticeCode) {
-		
-		return ResponseEntity
-				.ok()
+
+		return ResponseEntity.ok()
 				.body(new ResponseDTO(HttpStatus.OK, "조회 성공", noticeService.selectNoticeList(noticeCode)));
 	}
 
+	/* 파일 다운로드 */
+	@GetMapping("/download/{fileName}/{originName}")
+	public void download(@PathVariable String fileName, @PathVariable String originName, HttpServletResponse response) throws Exception {
+		try {
+			
+			String extension = originName.substring(originName.lastIndexOf("."));
+			
+			String path = "src/main/resources/static/productimgs/" + fileName + extension;
 
+			File file = new File(path);
+			response.setHeader("Content-Disposition", "attachment;filename=" + file.getName());
+
+			// 파일 읽어오기
+			FileInputStream fileInputStream = new FileInputStream(path);
+			OutputStream out = response.getOutputStream();
+
+			int read = 0;
+			byte[] buffer = new byte[1024];
+			// 1024바이트씩 계속 읽으면서 outputStream에 저장, -1이 나오면 더이상 읽을 파일이 없음
+			while ((read = fileInputStream.read(buffer)) != -1) {
+				out.write(buffer, 0, read);
+			}
+
+		} catch (Exception e) {
+			throw new Exception("download error");
+		}
+	}
 }
