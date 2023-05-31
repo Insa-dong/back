66e734efdfba2371fe862c943c13412abfd95376
rionqkd520@gmail.com <rionqkd520@gmail.com>
2023-05-31 22:20:29 +0900

    ✨ Feat: 휴직 검색

--- src/main/java/com/insadong/application/emporg/repository/RestRepository.java
+++ src/main/java/com/insadong/application/emporg/repository/RestRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestRepository  extends JpaRepository<Rest, Long> {
 
     Page<Rest> findByRestState(Pageable pageable, String searchKeyword);
+
+    Page<Rest> findByEmployeeEmpCodeIn(Pageable pageable, List<Long> empName);
 
 }
