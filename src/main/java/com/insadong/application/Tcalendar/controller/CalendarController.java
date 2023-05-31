0f50058367a25dd211a05074f144bc2c943f7081
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-05-31 17:49:52 +0900

    캘린더 끝

--- src/main/java/com/insadong/application/Tcalendar/controller/CalendarController.java
+++ src/main/java/com/insadong/application/Tcalendar/controller/CalendarController.java
@@ -38,6 +38,8 @@ public class CalendarController {
 	@GetMapping("/myPagingScheduleList")
 	public ResponseEntity<ResponseDTO> viewMyPagingCal(@AuthenticationPrincipal EmpDTOImplUS empDTO, @RequestParam(name = "page", defaultValue = "1") int page, @RequestParam(name = "sort", defaultValue = "calCode") String sort) {
 
+		log.info("sort : {} ", sort);
+
 		Page<CalendarDTO> data = calService.viewMyPagingScheduleList(empDTO.getEmpCode(), page, sort);
 		PagingButtonInfo pageInfo = Pagenation.getPagingButtonInfo(data);
 
@@ -74,5 +76,15 @@ public class CalendarController {
 
 		calService.registerMySchedule(calendar, writer);
 		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "등록 성공"));
+	}
+
+	@DeleteMapping("/mySchedule")
+	public ResponseEntity<ResponseDTO> deleteMySchedule(@RequestBody List<Long> codeList) {
+
+		log.info("codeList : {} ", codeList);
+
+		calService.deleteMySchedule(codeList);
+
+		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "삭제 완료"));
 	}
 }
